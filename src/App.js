1d3a794a42f386715b48ffe180a7daa7a495389c
zeeracha <zeeraratcha@gmail.com>
2022-07-21 13:36:38 +0700

    change app.js detail

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 
 import Routes from './config/Routes';
-
+//zeeracha
 function App() {
     return (
         <BrowserRouter>
             <Route render={props => (
                 <>
-                    <Header {...props}/>
-                    <Routes/>
-                    <Footer/>
+                    <Header {...props} />
+                    <Routes />
+                    <Footer />
                 </>
-            )}/>
+            )} />
         </BrowserRouter>
     );
 }
