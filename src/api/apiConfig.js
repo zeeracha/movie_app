9a4843df3fe0b4c09048cf99e54916d21d655cc8
zeeracha <zeeraratcha@gmail.com>
2022-07-21 14:47:31 +0700

    Updates

--- src/api/apiConfig.js
+++
@@ -1,8 +0,0 @@
-const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '7ac9358c989d413dea66ef3d2dbc07e8',
-    originalImage: (imgPath) => `https://image.tbpmmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
-}
-
-export default apiConfig;
